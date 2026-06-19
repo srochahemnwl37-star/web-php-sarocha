2725d7361e97d910de1d0ad2f216fe40e335e27f
srochahemnwl37-star <srochahemnwl37@gmail.com>
2026-06-19 12:54:41 +0700

    Create week5-receive.php

---
+++ week5-receive.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_GET['userName'];
+    $password = $_GET['userPass'];
+    $email = $_GET['userEmail'];
+    $date = $_GET['userdate'];
+    $age = $_GET['age'];
+    $gender = $_GET['userGender'];
+    $province = $_GET['province'];
+    $hobby = $_GET['userHobby'];;
+
+    echo "สวัสดีคุณ $name<br>";
+    echo "รหัสผ่านของคุณคือ: $password<br>";
+    echo "อีเมลของคุณคือ: $email<br>";
+    echo "วันเกิดของคุณคือ: $date<br>";
+    echo "อายุของคุณคือ: $age<br>";
+    echo "เพศของคุณคือ: $gender<br>";
+    echo "จังหวัดของคุณคือ: $province<br>";
+    echo "งานอดิเรกของคุณคือ:" . implode(", ", $hobby) . "<br>";
+?>
