31067a67016dae8525a3e1a47d590f6f88d82a60
srochahemnwl37-star <srochahemnwl37@gmail.com>
2026-06-19 12:54:44 +0700

    Create week5-send.php

---
+++ week5-send.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+    <form action="week5-receive.php" method="get ">
+        <label>username</label>
+        <input  type="text" name="userName">
+
+    <br>
+        <label>password</label>
+        <input type="password" name="userPass">
+<br>
+        <label>email</label>
+        <input type="email" name="userEmail">
+<br>
+        <label>วันเกิด</label>
+        <input type="date" name="userdate">
+<br>
+        <label>อายุ</label>
+        <input type="number" name="age">
+<br>
+        <input type="radio" name="userGender" value="ชาย"> ชาย
+        <input type="radio" name="userGender" value="หญิง"> หญิง
+<br>
+        <select name="province">
+            <option value="ไม่ระบุ">-</option>
+            <option value="กรุงเทพ">กรุงเทพ</option>
+        </select>
+<br>
+        <input type="checkbox" name="userHobby[]" value="อ่านหนังสือ"> อ่านหนังสือ
+        <input type="checkbox" name="userHobby[]" value="ฟังเพลง"> ฟังเพลง
+<br>
+
+         <input type="submit" value="ส่งข้อมูล">
+
+
+    </form>
+</body>
+</html>
