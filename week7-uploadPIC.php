7787e56655ee0f93ca9f9b18e5c7361ab74c5c17
srochahemnwl37-star <srochahemnwl37@gmail.com>
2026-07-07 12:31:19 +0700

    week

---
+++ week7-uploadPIC.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Upload Picture</title>
+</head>
+<body>
+    <form method="post" enctype="multipart/form-data" action="week7-showPic.php">
+        <input type="file" name="avatar">
+        <input type="submit" value="Upload">  
+        
+    </form>
+
+
+</body>
+</html>
---
+++ week8-setCookie.php
@@ -0,0 +1,4 @@
+<?php 
+    setcookie("user","sarocha",3600, "/");
+    echo "cookie ชื่อ: " . $_COOKIE["user"];
+?>
